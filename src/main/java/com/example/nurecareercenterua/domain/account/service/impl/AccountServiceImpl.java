e1b11043d655e6da758e1304fce5a858ebc3b1cd
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-05 19:51:07 +0200

    Create service AccountServiceImpl

---
+++ src/main/java/com/example/nurecareercenterua/domain/account/service/impl/AccountServiceImpl.java
@@ -0,0 +1,63 @@
+package com.example.nurecareercenterua.domain.account.service.impl;
+
+import com.example.nurecareercenterua.domain.account.exception.EmailAlreadyRegisteredException;
+import com.example.nurecareercenterua.domain.account.exception.IllegalPasswordArgumentException;
+import com.example.nurecareercenterua.domain.account.exception.PhoneAlreadyRegisteredException;
+import com.example.nurecareercenterua.domain.account.model.entity.Account;
+import com.example.nurecareercenterua.domain.account.model.enums.AccountRole;
+import com.example.nurecareercenterua.domain.account.model.request.RegistrationAccount;
+import com.example.nurecareercenterua.domain.account.model.response.CreatedAccount;
+import com.example.nurecareercenterua.domain.account.repository.AccountRepository;
+import com.example.nurecareercenterua.domain.account.service.AccountService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.UUID;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountServiceImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public CreatedAccount register(RegistrationAccount registrationAccount) {
+        if (accountRepository.existsAccountByEmail(registrationAccount.getEmail())) {
+            throw new EmailAlreadyRegisteredException();
+        } else if (accountRepository.existsAccountByPhone(registrationAccount.getPhone())) {
+            throw new PhoneAlreadyRegisteredException();
+        } else if (!registrationAccount.getConfirmedPassword().equals(registrationAccount.getPassword())) {
+            throw new IllegalPasswordArgumentException();
+        }
+
+        Account account = Account.builder()
+                .id(UUID.randomUUID())
+                .lastname(registrationAccount.getLastname())
+                .firstname(registrationAccount.getFirstname())
+                .patronymic(registrationAccount.getPatronymic())
+                .phone(registrationAccount.getPhone())
+                .email(registrationAccount.getEmail())
+                .password(registrationAccount.getPassword())
+                .role(registrationAccount.getRole())
+                .isActive(true)
+                .build();
+
+        Account createdAccount = accountRepository.save(account);
+
+        return new CreatedAccount(createdAccount.getId(),
+                createdAccount.getLastname(),
+                createdAccount.getFirstname(),
+                createdAccount.getPatronymic(),
+                createdAccount.getEmail(),
+                createdAccount.getPhone(),
+                createdAccount.getRole(),
+                new Date());
+    }
+}
