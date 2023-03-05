6ef177dd6c82cb73b84b4750d88d8c8e1880a45d
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-05 14:09:54 +0200

    Generate project

---
+++ src/main/java/com/example/nurecareercenterua/NureCareerCenterUaApplication.java
@@ -0,0 +1,13 @@
+package com.example.nurecareercenterua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NureCareerCenterUaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NureCareerCenterUaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nurecareercenterua/NureCareerCenterUaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nurecareercenterua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NureCareerCenterUaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
