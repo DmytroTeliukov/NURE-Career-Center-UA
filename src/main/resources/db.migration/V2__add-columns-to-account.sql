ed587fe282bb68bc42659353648b09bdbb0238cf
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-06 11:09:29 +0200

    Create sql-query for adding fields for table `accounts`

---
+++ src/main/resources/db.migration/V2__add-columns-to-account.sql
@@ -0,0 +1,3 @@
+alter table accounts add column joined timestamp not null;
+alter table accounts add column last_login timestamp not null;
+alter table accounts add column last_updated timestamp not null;
