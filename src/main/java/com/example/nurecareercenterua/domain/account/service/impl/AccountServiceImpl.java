c864363a61e4b3dddcd6c3af065a6797f137c344
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-07 10:43:23 +0200

    Implement method findAllByRole in service AccountServiceImpl

--- src/main/java/com/example/nurecareercenterua/domain/account/service/impl/AccountServiceImpl.java
+++ src/main/java/com/example/nurecareercenterua/domain/account/service/impl/AccountServiceImpl.java
@@ -3,28 +3,33 @@ package com.example.nurecareercenterua.domain.account.service.impl;
 import com.example.nurecareercenterua.domain.account.exception.EmailAlreadyRegisteredException;
 import com.example.nurecareercenterua.domain.account.exception.IllegalPasswordArgumentException;
 import com.example.nurecareercenterua.domain.account.exception.PhoneAlreadyRegisteredException;
+import com.example.nurecareercenterua.domain.account.mapper.AccountMapper;
+import com.example.nurecareercenterua.domain.account.model.dto.AccountDto;
 import com.example.nurecareercenterua.domain.account.model.entity.Account;
 import com.example.nurecareercenterua.domain.account.model.enums.AccountRole;
 import com.example.nurecareercenterua.domain.account.model.request.RegistrationAccount;
 import com.example.nurecareercenterua.domain.account.model.response.CreatedAccount;
 import com.example.nurecareercenterua.domain.account.repository.AccountRepository;
 import com.example.nurecareercenterua.domain.account.service.AccountService;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class AccountServiceImpl implements AccountService {
 
     private final AccountRepository accountRepository;
+    private final AccountMapper accountMapper;
 
     @Autowired
-    public AccountServiceImpl(AccountRepository accountRepository) {
+    public AccountServiceImpl(AccountRepository accountRepository, AccountMapper accountMapper) {
         this.accountRepository = accountRepository;
+        this.accountMapper = accountMapper;
     }
 
     @Override
@@ -56,6 +61,14 @@ public class AccountServiceImpl implements AccountService {
                 createdAccount.getPhone(),
                 createdAccount.getRole(),
                 createdAccount.getJoined());
+    }
+
+    @Override
+    public List<AccountDto> findAllByRole(AccountRole role, int page, int size) {
+        return accountRepository.findAllByAccountRole(role, PageRequest.of(page, size))
+                .stream()
+                .map(accountMapper::fromAccount)
+                .collect(Collectors.toList());
     }
 
     private void validateRegistrationData(RegistrationAccount registrationAccount) {
