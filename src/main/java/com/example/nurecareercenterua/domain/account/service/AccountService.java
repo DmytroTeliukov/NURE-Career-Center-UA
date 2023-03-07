f07f27fdb2a3d3fc0423cf17fb09b125084a52e6
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-07 10:42:30 +0200

    Add method findAllByRole in interface AccountService

--- src/main/java/com/example/nurecareercenterua/domain/account/service/AccountService.java
+++ src/main/java/com/example/nurecareercenterua/domain/account/service/AccountService.java
@@ -1,8 +1,15 @@
 package com.example.nurecareercenterua.domain.account.service;
 
+import com.example.nurecareercenterua.domain.account.model.dto.AccountDto;
+import com.example.nurecareercenterua.domain.account.model.entity.Account;
+import com.example.nurecareercenterua.domain.account.model.enums.AccountRole;
 import com.example.nurecareercenterua.domain.account.model.request.RegistrationAccount;
 import com.example.nurecareercenterua.domain.account.model.response.CreatedAccount;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 public interface AccountService {
     CreatedAccount register(RegistrationAccount registrationAccount);
+    List<AccountDto> findAllByRole(AccountRole role, int page, int size);
 }
