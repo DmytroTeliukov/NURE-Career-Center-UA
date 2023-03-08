b34433b83ac7eae5de1b52115603a416aec7e916
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-08 11:00:17 +0200

    Add method changeAccountAccess in interface AccountService

--- src/main/java/com/example/nurecareercenterua/domain/account/service/AccountService.java
+++ src/main/java/com/example/nurecareercenterua/domain/account/service/AccountService.java
@@ -3,6 +3,7 @@ package com.example.nurecareercenterua.domain.account.service;
 import com.example.nurecareercenterua.domain.account.model.dto.AccountDto;
 import com.example.nurecareercenterua.domain.account.model.entity.Account;
 import com.example.nurecareercenterua.domain.account.model.enums.AccountRole;
+import com.example.nurecareercenterua.domain.account.model.request.AccountOperationRequest;
 import com.example.nurecareercenterua.domain.account.model.request.RegistrationAccount;
 import com.example.nurecareercenterua.domain.account.model.response.CreatedAccount;
 import org.springframework.data.domain.Page;
@@ -12,4 +13,5 @@ import java.util.List;
 public interface AccountService {
     CreatedAccount register(RegistrationAccount registrationAccount);
     List<AccountDto> findAllByRole(AccountRole role, int page, int size);
+    void changeAccountAccess(AccountOperationRequest accountOperationRequest);
 }
