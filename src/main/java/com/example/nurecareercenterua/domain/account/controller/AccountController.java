6330b4eb379d8317920a0b2ee211a7e1f2e21ac9
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-08 12:10:52 +0200

    Add endpoint /password in controller AccountController

--- src/main/java/com/example/nurecareercenterua/domain/account/controller/AccountController.java
+++ src/main/java/com/example/nurecareercenterua/domain/account/controller/AccountController.java
@@ -1,11 +1,13 @@
 package com.example.nurecareercenterua.domain.account.controller;
 
+import com.example.nurecareercenterua.domain.account.exception.AccountNotFoundException;
 import com.example.nurecareercenterua.domain.account.exception.EmailAlreadyRegisteredException;
 import com.example.nurecareercenterua.domain.account.exception.IllegalPasswordArgumentException;
 import com.example.nurecareercenterua.domain.account.exception.PhoneAlreadyRegisteredException;
 import com.example.nurecareercenterua.domain.account.model.dto.AccountDto;
 import com.example.nurecareercenterua.domain.account.model.enums.AccountRole;
 import com.example.nurecareercenterua.domain.account.model.request.AccountOperationRequest;
+import com.example.nurecareercenterua.domain.account.model.request.ChangePasswordRequest;
 import com.example.nurecareercenterua.domain.account.model.request.RegistrationAccount;
 import com.example.nurecareercenterua.domain.account.model.response.CreatedAccount;
 import com.example.nurecareercenterua.domain.account.service.AccountService;
@@ -47,10 +49,26 @@ public class AccountController {
         return ResponseEntity.noContent().build();
     }
 
+    @PatchMapping("/password")
+    public ResponseEntity<?> changePassword(@RequestBody ChangePasswordRequest changePasswordRequest) {
+        accountService.changePassword(changePasswordRequest);
+        return ResponseEntity.noContent().build();
+    }
+
     @ExceptionHandler({EmailAlreadyRegisteredException.class,
-            PhoneAlreadyRegisteredException.class,
-            IllegalPasswordArgumentException.class})
+            PhoneAlreadyRegisteredException.class})
     public ResponseEntity<?> handlerAccountException(Exception e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
     }
+
+    @ExceptionHandler(IllegalPasswordArgumentException.class)
+    public ResponseEntity<?> handlerPasswordException(Exception e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AccountNotFoundException.class)
+    public ResponseEntity<?> handlerNotFoundAccountException(Exception e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
 }
