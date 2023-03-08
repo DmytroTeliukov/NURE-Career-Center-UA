f242302ca9ff6be0bb44fd4650feda497c5f53f2
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-08 11:57:07 +0200

    Change constructor in exception IllegalPasswordArgumentException

--- src/main/java/com/example/nurecareercenterua/domain/account/exception/IllegalPasswordArgumentException.java
+++ src/main/java/com/example/nurecareercenterua/domain/account/exception/IllegalPasswordArgumentException.java
@@ -1,7 +1,5 @@
 package com.example.nurecareercenterua.domain.account.exception;
 
 public class IllegalPasswordArgumentException extends RuntimeException {
-    public IllegalPasswordArgumentException () {
-        super("Incorrect password");
-    }
+    public IllegalPasswordArgumentException(String message) {super(message);}
 }
