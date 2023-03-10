33b10b8ce61d50cd558e89ada8420079f6a56c4e
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-10 14:46:29 +0200

    Create controller AdminController

---
+++ src/main/java/com/example/nurecareercenterua/controller/AdminController.java
@@ -0,0 +1,18 @@
+package com.example.nurecareercenterua.controller;
+
+import com.example.nurecareercenterua.domain.account.model.request.RegistrationAccount;
+import com.example.nurecareercenterua.domain.account.model.response.CreatedAccount;
+import com.example.nurecareercenterua.domain.account.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admins")
+public class AdminController {
+
+
+}
