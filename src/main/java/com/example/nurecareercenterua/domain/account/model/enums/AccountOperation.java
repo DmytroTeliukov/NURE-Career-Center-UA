c63daae30e90730fdd7c205ccbdf7fd3fb57c471
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-12 11:16:48 +0200

    Change enum AccessOperation

--- src/main/java/com/example/nurecareercenterua/domain/account/model/enums/AccountOperation.java
+++ src/main/java/com/example/nurecareercenterua/domain/account/model/enums/AccountOperation.java
@@ -1,15 +1,5 @@
 package com.example.nurecareercenterua.domain.account.model.enums;
 
 public enum AccountOperation {
-    RESTORE(true), BAN(false);
-    private final boolean content;
-
-    AccountOperation(boolean content) {
-        this.content = content;
-    }
-
-
-    public boolean getContent() {
-        return content;
-    }
+    RESTORE, BAN
 }
