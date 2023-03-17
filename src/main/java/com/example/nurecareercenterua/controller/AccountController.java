363611fc392e5dbc9af997a88d6afa0140a8bf80
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-17 09:44:17 +0200

    Change controller AccountController

--- src/main/java/com/example/nurecareercenterua/controller/AccountController.java
+++ src/main/java/com/example/nurecareercenterua/controller/AccountController.java
@@ -1,34 +1,18 @@
 package com.example.nurecareercenterua.controller;
 
-import com.example.nurecareercenterua.domain.account.exception.AccountNotFoundException;
-import com.example.nurecareercenterua.domain.account.exception.EmailAlreadyRegisteredException;
-import com.example.nurecareercenterua.domain.account.exception.IllegalPasswordArgumentException;
-import com.example.nurecareercenterua.domain.account.exception.PhoneAlreadyRegisteredException;
-import com.example.nurecareercenterua.domain.account.model.dto.AccountDto;
-import com.example.nurecareercenterua.domain.account.model.enums.AccountRole;
 import com.example.nurecareercenterua.domain.account.model.request.AccountOperationRequest;
 import com.example.nurecareercenterua.domain.account.model.request.ChangePasswordRequest;
-import com.example.nurecareercenterua.domain.account.model.request.RegistrationAccount;
-import com.example.nurecareercenterua.domain.account.model.response.CreatedAccount;
 import com.example.nurecareercenterua.domain.account.service.AccountService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/accounts")
 public class AccountController {
 
     private final AccountService accountService;
-
-    @Autowired
-    public AccountController(AccountService accountService) {
-        this.accountService = accountService;
-    }
 
     @PostMapping("/access")
     public ResponseEntity<?> changeAccountAccess(@RequestBody AccountOperationRequest accountOperationRequest) {
