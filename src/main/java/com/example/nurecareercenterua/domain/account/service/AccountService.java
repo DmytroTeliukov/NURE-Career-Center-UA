40e1557ee4a99d752638986064c1f5b6004010da
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-18 12:25:46 +0200

    Update structure of code

--- src/main/java/com/example/nurecareercenterua/domain/account/service/AccountService.java
+++ src/main/java/com/example/nurecareercenterua/domain/account/service/AccountService.java
@@ -5,7 +5,7 @@ import com.example.nurecareercenterua.domain.account.model.enums.AccountRole;
 import com.example.nurecareercenterua.domain.account.model.dto.request.AccountOperationRequest;
 import com.example.nurecareercenterua.domain.account.model.dto.request.ChangePasswordRequest;
 import com.example.nurecareercenterua.domain.account.model.dto.request.RegistrationAccount;
-import com.example.nurecareercenterua.domain.account.model.dto.response.CreatedAccount;
+import com.example.nurecareercenterua.domain.account.model.dto.request.response.CreatedAccount;
 
 import java.util.List;
 
