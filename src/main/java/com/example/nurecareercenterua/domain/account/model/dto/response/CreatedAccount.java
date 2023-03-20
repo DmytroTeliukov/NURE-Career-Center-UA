5a3fb4a0ae9ab96de33b9ae56087d9d78996bb1f
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-20 12:50:32 +0200

    Add photoUrl in response CreatedAccount

--- src/main/java/com/example/nurecareercenterua/domain/account/model/dto/response/CreatedAccount.java
+++ src/main/java/com/example/nurecareercenterua/domain/account/model/dto/response/CreatedAccount.java
@@ -1,4 +1,4 @@
-package com.example.nurecareercenterua.domain.account.model.dto.request.response;
+package com.example.nurecareercenterua.domain.account.model.dto.response;
 
 import com.example.nurecareercenterua.domain.account.model.enums.AccountRole;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -11,6 +11,7 @@ public record CreatedAccount(
         @JsonProperty("lastname") String lastname,
         @JsonProperty("firstname") String firstname,
         @JsonProperty("patronymic") String patronymic,
+        @JsonProperty("avatar") String photoUrl,
         @JsonProperty("email") String email,
         @JsonProperty("phone") String phone,
         @JsonProperty("role") AccountRole role,
