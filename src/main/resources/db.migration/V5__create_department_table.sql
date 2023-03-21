2063b204169bd20b2101a116ff43838465795964
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-21 18:53:35 +0200

    Create tables department and specialty

---
+++ src/main/resources/db.migration/V5__create_department_table.sql
@@ -0,0 +1,4 @@
+create table department(
+    short_name varchar(8) primary key,
+    full_name varchar(100)
+)ENGINE = InnoDB;
---
+++ src/main/resources/db.migration/V6__create_specialty_table.sql
@@ -0,0 +1,4 @@
+create table specialty(
+    sp_number int primary key,
+    full_name varchar(100)
+)ENGINE = InnoDB;
